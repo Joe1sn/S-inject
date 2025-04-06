93ad20ada512efd4023794fca59069aeeb3e150f
joe1sn <joe1sn23333@gmail.com>
2025-04-06 11:01:38 +0800

    feat: new branch & lib version

---
+++ InjectLib/InjectLib.cpp
@@ -0,0 +1,87 @@
+﻿// InjectLib.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "InjectLib.h"
+
+//远程线程注入DLL
+INJECTLIB_API bool rmtdll(std::string dllPath, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector(dllPath);
+    injector.remoteThreadInject(pid);
+    return true;
+}
+
+//反射式注入DLL
+INJECTLIB_API bool refdll(std::string dllPath, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector(dllPath);
+    injector.reflectInject(pid);
+    return true;
+}
+//APC队列注入DLL
+INJECTLIB_API bool apcdll(std::string dllPath, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector(dllPath);
+    injector.apcInject(pid);
+    return true;
+}
+//从网络加载DLL注入DLL
+INJECTLIB_API bool net(std::string dllPath, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector(dllPath);
+    injector.internetInject(pid, dllPath);
+    return true;
+}
+//远程线程注入Shellcode
+INJECTLIB_API bool rmtsc(std::string shellcode, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector();
+    injector.shellcodeInject(shellcode, pid);
+    return true;
+}
+//APC队列注入Shellcode
+INJECTLIB_API bool apcsc(std::string shellcode, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector();
+    injector.apcShellcodeInject(shellcode, pid);
+    return true;
+}
+//上下文注入Shellcode
+INJECTLIB_API bool ctxsc(std::string shellcode, DWORD pid) {
+    if (pid == 0)
+        return false;
+    auto injector = Injector();
+    injector.contextShellcodeInject(shellcode, pid);
+    return true;
+}
+
+//上下文注入Shellcode
+INJECTLIB_API DWORD getPID(const char* proc_name_cstr) {
+    auto injector = Injector();
+    return injector.getPidByName(proc_name_cstr);
+}
+
+/*
+// 这是导出变量的一个示例
+INJECTLIB_API int nInjectLib=0;
+
+// 这是导出函数的一个示例。
+INJECTLIB_API int fnInjectLib(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+CInjectLib::CInjectLib()
+{
+    return;
+}
+*/
---
+++ InjectLib/InjectLib.h
@@ -0,0 +1,39 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 INJECTLIB_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// INJECTLIB_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef INJECTLIB_EXPORTS
+#define INJECTLIB_API __declspec(dllexport)
+#else
+#define INJECTLIB_API __declspec(dllimport)
+#endif
+
+#include "./app/Injector.h"
+
+#include <iostream>
+#include <string>
+
+
+extern "C" INJECTLIB_API bool rmtdll(std::string dllPath, DWORD pid);
+extern "C" INJECTLIB_API bool refdll(std::string dllPath, DWORD pid);
+extern "C" INJECTLIB_API bool apcdll(std::string dllPath, DWORD pid);
+extern "C" INJECTLIB_API bool net(std::string dllPath, DWORD pid);
+extern "C" INJECTLIB_API bool rmtsc(std::string shellcode, DWORD pid);
+extern "C" INJECTLIB_API bool apcsc(std::string shellcode, DWORD pid);
+extern "C" INJECTLIB_API bool ctxsc(std::string shellcode, DWORD pid);
+extern "C" INJECTLIB_API DWORD getPID(const char* proc_name_cstr);
+
+/*
+// 此类是从 dll 导出的
+class INJECTLIB_API CInjectLib {
+public:
+	CInjectLib(void);
+	// TODO: 在此处添加方法。
+};
+
+extern INJECTLIB_API int nInjectLib;
+
+INJECTLIB_API int fnInjectLib(void);
+*/
